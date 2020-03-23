7cb232a503e9f712b9b58d3c936002b1c6746f09
David Mak <david.18.19.21@gmail.com>
2020-03-23 22:26:02 +0800

    [BookPutHandler] Fix missing check for Method
    Caught by a WIP test suite.

--- src/main/java/comp4111/handler/BooksPutHandler.java
+++ src/main/java/comp4111/handler/BooksPutHandler.java
@@ -46,6 +46,8 @@ public abstract class BooksPutHandler extends HttpEndpointHandler {
 
     @Override
     public void handle(ClassicHttpRequest request, ClassicHttpResponse response, HttpContext context) throws HttpException, IOException {
+        checkMethod(request, response);
+
         final var queryParams = HttpUtils.parseQueryParams(request.getPath(), response);
         final var token = checkToken(queryParams, response);
 
