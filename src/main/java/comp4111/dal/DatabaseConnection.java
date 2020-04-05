774c18d05c789c89107df427d563bf1074532949
David Mak <david.18.19.21@gmail.com>
2020-04-06 01:22:08 +0800

    [DatabaseConnection] Fix unable to create 'comp4111' database
    Connection pool creation depended on the existence of the database, but
    it was initialized before the database is.
    
    This only manifested with a fresh MySQL server container.

--- src/main/java/comp4111/dal/DatabaseConnection.java
+++ src/main/java/comp4111/dal/DatabaseConnection.java
@@ -36,7 +36,6 @@ public class DatabaseConnection {
         try {
             // The connection is supposed to be closed in MainApplication.
             con = DriverManager.getConnection(MYSQL_URL, MYSQL_LOGIN, MYSQL_PASSWORD);
-            connectionPool = new DatabaseConnectionPool(MYSQL_URL, DB_NAME, MYSQL_LOGIN, MYSQL_PASSWORD);
             if (!databaseExists(con, DB_NAME)) {
                 createDatabase(con, DB_NAME);
                 useDatabase(con, DB_NAME);
@@ -60,6 +59,7 @@ public class DatabaseConnection {
                         ");";
                 createTable(con, tableSpec);
             }
+            connectionPool = new DatabaseConnectionPool(MYSQL_URL, DB_NAME, MYSQL_LOGIN, MYSQL_PASSWORD);
         } catch (Exception e) {
             LOGGER.error("Error setting up the environment", e);
             System.exit(1);
