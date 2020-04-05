c54d7305cc6bfa3d4f6b248563feb689f3ed8e60
David Tang <davidtang1006@gmail.com>
2020-04-05 19:35:27 +0800

    Cancel timeout transactions (the timeout is set to be 90 seconds)
    Check the IDE console for the message "A timeout transaction is cancelled".

--- src/main/java/comp4111/dal/DatabaseConnectionPool.java
+++ src/main/java/comp4111/dal/DatabaseConnectionPool.java
@@ -1,18 +1,25 @@
 package comp4111.dal;
 
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 // https://www.baeldung.com/java-connection-pooling
 public class DatabaseConnectionPool {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConnectionPool.class);
+
     private List<Connection> connectionPool;
     private List<Connection> usedConnections = new ArrayList<>();
+    private List<Timer> timers = new ArrayList<>();
     private static final int INITIAL_POOL_SIZE = 10;
 
     public DatabaseConnectionPool(@NotNull String url, @NotNull String database, @NotNull String user, @NotNull String password) throws SQLException {
@@ -34,17 +41,47 @@ public class DatabaseConnectionPool {
         return con;
     }
 
+    private class TimerTaskImpl extends TimerTask {
+
+        private Connection con;
+
+        private TimerTaskImpl(@NotNull Connection con) {
+            this.con = con;
+        }
+
+        @Override
+        public void run() {
+            try {
+                con.rollback();
+                releaseConnection(con);
+                LOGGER.info("A timeout transaction is cancelled");
+            } catch (SQLException e) {
+                LOGGER.error("Error rolling back", e);
+            }
+        }
+    }
+
     public Connection getConnection() {
         if (connectionPool.size() != 0) {
             Connection connection = connectionPool
                     .remove(connectionPool.size() - 1);
+
+            Timer timer = new Timer();
+            timer.schedule(new TimerTaskImpl(connection), 90000);
+
             usedConnections.add(connection);
+            timers.add(timer);
             return connection;
         }
         return null;
     }
 
     public void releaseConnection(@NotNull Connection connection) {
+        int index = usedConnections.indexOf(connection);
+        Timer timer = timers.get(index);
+        timer.cancel();
+        timers.remove(timer);
+
         connectionPool.add(connection);
         usedConnections.remove(connection);
     }
